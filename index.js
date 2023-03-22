595df034fd4591f8654d4f35771516d7fab04021
+ <+>
2023-03-22 14:48:22 -0400

    no scope!

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(bladee = "roller-skate") {
+  return `This Saturday, I want to ${bladee}!`;
+
+}
+
+function mondayWork(ughWork = "go to the office") {
+    return `This Monday, I will ${ughWork}.`
+
+}
+
+function wrapAdjective(Mike = "*"){
+ return function(addjective = "special") {
+    return `You are ${Mike}${addjective}${Mike}!`
+ }
+
+
+}
